f7f93f4b0e1c8a0e0048df27f69a330ed175a5db
davidrynes <labs@davidrynes.com>
2025-12-07 22:01:10 +0100

    Fix sanity image.ts import for Vercel build

--- sanity/image.ts
+++ sanity/image.ts
@@ -1,13 +1,11 @@
-import { createImageUrlBuilder } from '@sanity/image-url'
-import { SanityImageSource } from '@sanity/image-url/lib/types/types'
-
+import imageUrlBuilder from '@sanity/image-url'
 import { dataset, projectId } from './env'
 
-const imageBuilder = createImageUrlBuilder({
+const imageBuilder = imageUrlBuilder({
   projectId: projectId || '',
   dataset: dataset || '',
 })
 
-export const urlForImage = (source: SanityImageSource) => {
+export const urlForImage = (source: any) => {
   return imageBuilder?.image(source).auto('format').fit('max')
 }
